d0621bdf06557f7bd5fe2256dc4a2d35a04f8aed
jjram20 <jde.ram9@gmail.com>
2025-08-06 15:37:56 -0600

    Correction typing handler createExcel

--- amplify/functions/create_excel/handler.ts
+++ amplify/functions/create_excel/handler.ts
@@ -1,3 +1,20 @@
-export const handler = async (event) => {
-    return "First function";
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { APIGatewayEvent, Context } from "aws-lambda";
+import { storageBucket } from '../../storage/resource';
+import * as XLSX from 'xlsx';
+
+export const handler = async (event: APIGatewayEvent) => {
+    const workbook = XLSX.utils.book_new();
+
+    const worksheet = XLSX.utils.aoa_to_sheet([
+        ['Nombre', 'Edad'],
+        ['Juanito',, 67]
+    ]);
+
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Nombres");
+    const currentDate = new Date().toISOString().replace("T", "-").slice(0, 16);
+    const fileName = `public/nombres_${currentDate}.xlsx`
+
+    const buffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    return "File created";
 }
