da48f29963a0113529b9793e32d115bee377e74c
jjram20 <jde.ram9@gmail.com>
2025-08-06 15:44:01 -0600

    Commeting XLSX functionality

--- amplify/functions/create_excel/handler.ts
+++ amplify/functions/create_excel/handler.ts
@@ -1,10 +1,10 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { APIGatewayEvent, Context } from "aws-lambda";
 import { storageBucket } from '../../storage/resource';
-import * as XLSX from 'xlsx';
+// import * as XLSX from 'xlsx';
 
 export const handler = async (event: APIGatewayEvent) => {
-    const workbook = XLSX.utils.book_new();
+    /*const workbook = XLSX.utils.book_new();
 
     const worksheet = XLSX.utils.aoa_to_sheet([
         ['Nombre', 'Edad'],
@@ -16,5 +16,6 @@ export const handler = async (event: APIGatewayEvent) => {
     const fileName = `public/nombres_${currentDate}.xlsx`
 
     const buffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    */
     return "File created";
 }
